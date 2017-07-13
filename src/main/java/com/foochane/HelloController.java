8b03904f875d8f8bc7767f1d6e71a469dd743099
foochane <fuchengkm@qq.com>
2017-07-13 19:55:15 +0800

    Controller使用

--- src/main/java/com/foochane/HelloController.java
+++ src/main/java/com/foochane/HelloController.java
@@ -1,22 +1,23 @@
 package com.foochane;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by fucheng on 2017/7/13.
  */
+//@RestController 等同意@Controller + @ResponseBody
 @RestController
 public class HelloController {
 
     @Autowired
     private  PersonProperties personProperties;
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String say(){
-        return personProperties.getCupSize();
+    @RequestMapping(value = {"say/{id}"},method = RequestMethod.GET)
+    public String say(@RequestParam(value = "id",required = false,defaultValue="0") Integer id){
+        //return personProperties.getCupSize();
+        //return "index";
+        return "id:"+id;
     }
 
 
